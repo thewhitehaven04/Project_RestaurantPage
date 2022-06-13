7ca752b009fdd81de9b43718d562409efa1d5f5f
thewhitehaven04 <thewhitehaven04@gmail.com>
2022-06-13 14:43:25 +0300

    styling complete

--- src/main/home/home.js
+++ src/main/home/home.js
@@ -6,22 +6,32 @@ export default function home() {
   const _styleClasses = {
     textCentered: 'flex-title',
     title: 'title',
+    image: 'image-main-scalable',
   };
+
+  function _renderImageResponsive(width, height) {
+    const img = new Image();
+    img.src = logo;
+    img.classList.add(_styleClasses.image);
+    
+    return img; 
+  }
+
+  function _renderTitle(titleText) {
+    const divText = document.createElement('div');
+    divText.textContent = titleText; 
+    divText.classList.add(_styleClasses.title);
+    return divText
+  }
 
   function render() {
     const div = document.createElement('div');
     div.classList.add(_styleClasses.textCentered);
 
-    const divText = document.createElement('div');
-    divText.textContent = 'Fish corner';
-    divText.classList.add(_styleClasses.title);
-
-    const img = new Image(600, 600);
-    img.src = logo;
-
-    div.appendChild(divText);
-    div.appendChild(img);
+    div.appendChild(_renderTitle('Fish Corner'));
+    div.appendChild(_renderImageResponsive(500, 500));
     
+    console.dir(div);
     return div;
   }
 
--- src/main/menu/menu.js
+++ src/main/menu/menu.js
@@ -2,6 +2,7 @@ import menuRepository from './menuCard/model/menuRepository';
 import menuCard from './menuCard/menuCard';
 import style from './menu.css';
 import { SoftShadowed } from '../../generic/shadowed/shadowed';
+import { Theme } from '../../generic/theme/theme';
 
 export default function menu(menuEntryCount) {
   const _styleClasses = {
@@ -10,8 +11,12 @@ export default function menu(menuEntryCount) {
   };
 
   function render() {
+    const divContainer = document.createElement('div');
     const div = document.createElement('div');
-    div.classList.add(...[_styleClasses.gridMenu, _styleClasses.menuStyle]);
+
+    divContainer.appendChild(div);
+    divContainer.classList.add(_styleClasses.menuStyle);
+    div.classList.add(_styleClasses.gridMenu);
 
     menuRepository()
       .getItems(0, menuEntryCount)
@@ -19,7 +24,7 @@ export default function menu(menuEntryCount) {
         div.appendChild(menuCard(model).render());
       });
 
-    return SoftShadowed(div).wrap();
+    return Theme(SoftShadowed(divContainer).wrap()).apply('white');
   }
 
   return { render };
